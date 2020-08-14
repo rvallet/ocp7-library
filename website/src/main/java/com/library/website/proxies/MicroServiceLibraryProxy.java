b5662748f3c01d6c168d9e9dd5788ffdda6374f0
rvallet <remy.vallet@gmail.com>
2020-08-14 14:51:36 +0200

    Ribbon Enable

--- website/src/main/java/com/library/website/proxies/MicroServiceLibraryProxy.java
+++ website/src/main/java/com/library/website/proxies/MicroServiceLibraryProxy.java
@@ -2,12 +2,14 @@ package com.library.website.proxies;
 
 import com.library.website.beans.UserBean;
 import com.library.website.service.UserService;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "ms-library", url="localhost:9001")
+@FeignClient(name = "ms-library")
+@RibbonClient(name = "ms-library")
 public interface MicroServiceLibraryProxy {
 
     @GetMapping(value= "/users")
