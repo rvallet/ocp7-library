56ca07d81cede38474b6aa8b3193a2d5a00d3ab2
rvallet <remy.vallet@gmail.com>
2020-10-10 15:46:39 +0200

    clean ms-batch

--- ms-batch/src/main/java/com/library/msbatch/MsBatchApplication.java
+++ ms-batch/src/main/java/com/library/msbatch/MsBatchApplication.java
@@ -1,11 +1,7 @@
 package com.library.msbatch;
 
-import com.library.msbatch.beans.BookLoanBean;
 import com.library.msbatch.config.ApplicationPropertiesConfig;
-import com.library.msbatch.config.EmailConfig;
 import com.library.msbatch.config.MailProperties;
-import com.library.msbatch.proxies.MicroServiceLibraryProxy;
-import com.library.msbatch.service.EmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +12,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.mail.SimpleMailMessage;
-
-import java.util.List;
 
 @SpringBootApplication(scanBasePackages="com.library")
 @EnableConfigurationProperties
@@ -36,11 +30,11 @@ public class MsBatchApplication implements CommandLineRunner {
 	@Autowired
 	SimpleMailMessage simpleMailMessage;
 
-	@Autowired
+/*	@Autowired
 	EmailConfig emailConfig;
 
 	@Autowired
-	EmailService emailService;
+	EmailService emailService;*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsBatchApplication.class, args);
@@ -51,8 +45,8 @@ public class MsBatchApplication implements CommandLineRunner {
 		LOGGER.info("\nAppProperties => {}", applicationPropertiesConfig.getExemple());
 		LOGGER.info("\nMailProperties => \nHost : {} \nPort : {} \nUserName: {} \nPassword : {}",mailProperties.getHost(), mailProperties.getPort(), mailProperties.getUsername(), mailProperties.getPassword());
 
-		String text = String.format(simpleMailMessage.getText(), emailConfig.template(), "<h1>Coucou</h1>");
-		emailService.sendSimpleMessage("remy.vallet@gmail.com", "subject", text);
+/*		String text = String.format(simpleMailMessage.getText(), emailConfig.template(), "<h1>Coucou</h1>");
+		emailService.sendSimpleMessage("remy.vallet@gmail.com", "subject", text);*/
 
 	}
 }
